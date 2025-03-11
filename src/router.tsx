69ae296327c5a451ce97672070533b498ec98b35
Vincent <oa5136@gmail.com>
2025-03-11 11:10:18 +0800

    delete old router settings

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,15 @@
-import { createBrowserRouter } from "react-router-dom";
-import Women from '@/pages/Women/index.tsx'
-import Men from '@/pages/Men/index.tsx'
-import Kids from '@/pages/Kids/index.tsx'
-import FindAStore from '@/pages/FindAStore/index.tsx'
+// import { createBrowserRouter } from "react-router-dom";
+// import Women from '@/pages/Women/index.tsx'
+// import Men from '@/pages/Men/index.tsx'
+// import Kids from '@/pages/Kids/index.tsx'
+// import FindAStore from '@/pages/FindAStore/index.tsx'
 
-const router = createBrowserRouter([
-    { path: "/", element: <Women /> },
-    { path: "/men", element: <Men /> },
-    { path: "/kids", element: <Kids /> },
-    { path: "/findAStore", element: <FindAStore /> },
-    // { path: "*", element: <NotFound /> }, // 404 頁面
-  ]);
+// const router = createBrowserRouter([
+//     { path: "/", element: <Women /> },
+//     { path: "/men", element: <Men /> },
+//     { path: "/kids", element: <Kids /> },
+//     { path: "/findAStore", element: <FindAStore /> },
+//     // { path: "*", element: <NotFound /> }, // 404 頁面
+//   ]);
   
-export default router;
+// export default router;
